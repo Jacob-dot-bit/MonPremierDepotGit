252bf5e47e0cbcef793297016123a85968efadd7
Chibrox <Chibrox@github.com>
2025-02-04 22:10:47 +0100

    config

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php 
+// config.php
+return [
+   'ia_used' => 'gemini',//openai or gemini
+   'api_key_open_ai' => 'sk-votre-clé-api-ici',
+   'api_key_gemini' => 'AIzaSyAHqQuZ2EdWf3D6mUYveGaTEv6qGortYvw',
+   'gemini_model' => 'gemini-1.5-flash-8b' // gemini-1.5-flash-8b or gemini-1.5-flash-latest
+];
